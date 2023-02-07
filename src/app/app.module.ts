b4e66b7c0bcffa08792ab2589082933a8ba38066
kPlusPlus <kresimir.ivkovic@gmail.com>
2023-02-07 20:27:12 +0100

    zajebi http request ali neradi još

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'; //TODO:test this
+import { map } from 'rxjs/operators';
+
+
 
 
 
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 //import { RequestOptions } from 'http';
 import { HttpHeaders } from '@angular/common/http';
+//import { map } from 'rxjs/operators';
+
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
 declare let $: any;
 
 
@@ -13,23 +18,27 @@ declare let $: any;
 })
 export class LoginPage implements OnInit {
 
+  //serverurl = 'http://kplusplus.ddns.net:8080/';
+  private serverurl: any = 'http://ubuntu:8080/';
+  private useremail = '';
+  private userpsw = '';
+  private userid = '';
+  private res = "";
+
   constructor(public httpClient: HttpClient) { }
 
-  ngOnInit() {
-    const ime = 'kreso';
+  ngOnInit() { }
 
-    /*
-    $(document).ready(function () {
-      alert("Hello from New to design")
-    });
-    */
+  getGetFunction() {
+    this.httpClient.get('http://someapi')
+      .pipe(map(res => res));
   }
 
-  serverurl = 'http://kplusplus.ddns.net:8080/';
-  useremail = '';
-  userpsw = '';
-  userid = '';
-  //http: HttpClient;
+  getPostFunction(yourPara) {
+    this.httpClient.get('http://someapi', yourPara)
+      .pipe(map(res => res));
+  }
+
 
 
   getFilms() {
@@ -38,26 +47,38 @@ export class LoginPage implements OnInit {
 
   btnClicked() {
 
-    //this.useremail = $('[name=\'useremail\']').val();
-    //this.userpsw = $('[name=\'userpsw\']').val();
-
-    alert(this.userpsw);
-    alert(this.userid);
+    //alert(this.userpsw);
+    //alert(this.userid);
     //this.userid =  this.httpClient.post(this.serverurl + 'cmemb.php' )
 
-
-    const body = { user: this.useremail, password: this.userpsw };
-
+    const body = { useremail: this.useremail, userpsw: this.userpsw };
 
     //HttpClient.post(this.serverurl + 'cmemb.php' );
     const data = 'user=' + this.useremail + '&password=' + this.userpsw;
 
     const headers = new HttpHeaders();
     headers.append('Accept', 'application/json');
-    headers.append('Content-Type', 'application/json' );
+    headers.append('Content-Type', 'application/json');
 
-    this.httpClient.post(this.serverurl + 'cmemb.php', data, { headers });
+    this.httpClient.post(this.serverurl + 'cmemb.php', data, { headers }).subscribe(datas => data);
 
+    console.log(this.serverurl + 'cmemb.php');
+    console.log(headers);
+
+
+    this.httpClient.get('https://randomuser.me/api/?results=10')
+      .pipe(map(res => this.res));
+    console.log('******************************');
+    console.log(this.res);
+/*
+    this.httpClient.get('https://randomuser.me/api/?results=10')
+      .map(res => res.json())
+      .subscribe(res => {
+        this.userid = res.results;
+      }, (err) => {
+        alert("failed loading json data");
+      });
+*/
 
     //$('#userid').val(555);
     /*
